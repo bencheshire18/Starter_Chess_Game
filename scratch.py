646bd8ee27a4220ce3fd7e2401f3b67425308d54
bencheshire18 <bencheshire18@gmail.com>
2026-09-18 16:41:46 +0100

    Scratch change

--- scratch.py
+++ scratch.py
@@ -1,26 +1,22 @@
-import pygame
-from pathlib import Path
+import random
+import sys
 
-script_dir = Path(__file__).parent
-file_path = script_dir / "ui" / "Piece_Sprites" / "whiteQueen.png"
+for i in range(10000):
+    all_legal_moves = [
+        [[0, 0], [0, 1]], [[0, 0], [0, 2]], [[0, 0], [0, 3]],
+        [[1, 0], [1, 4]], [[1, 0], [1, 5]], [[1, 0], [1, 6]],
+        [[2, 0], [2, 7]], [[2, 0], [2, 8]], [[2, 0], [2, 9]],
+    ]
 
-pygame.init()
+    # pick a random starter from all_legal_moves
+    starter = random.choice(all_legal_moves)[0]
 
-win_icon = pygame.image.load(file_path)
-pygame.display.set_icon(win_icon)
+    # pick a random end that relates to that starter
+    options = [move[1] for move in all_legal_moves if move[0] == starter]
+    end = random.choice(options)
 
-WIDTH, HEIGHT = 800, 400
-FPS = 60
-CLOCK = pygame.time.Clock()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    if starter[0] != end[0]:
+        print("Failed!!")
+        sys.exit()
 
-running = True
-counter = 0
-while running:
-    CLOCK.tick(FPS)
-    counter += 1
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+print("Succeeded")
